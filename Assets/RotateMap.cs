a09541b377ef7d3f7eeda1a76bb80495da6c1555
Stien_the_Troller <stijnkaay@gmail.com>
2021-02-15 01:00:29 +0100

    turn is now gradual

--- Assets/RotateMap.cs
+++ Assets/RotateMap.cs
@@ -7,11 +7,14 @@ public class RotateMap : MonoBehaviour
     public GameObject Map;
 
     public float angle = 90;
+    public float RotationSpeed = 0.000001f;
+    public Quaternion wantedRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         Map = GameObject.Find("Map");
+        wantedRotation = Quaternion.LookRotation(Map.transform.position);
     }
 
     //put it inside a for loop with a delta timer to make a smooth motion out of it.
@@ -20,21 +23,31 @@ public class RotateMap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
+        
+        
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Map.transform.Rotate(0, 0, angle);
+
+            //Map.transform.Rotate(0, 0, angle);
+            wantedRotation = Quaternion.Euler(Vector3.forward * -angle);
         }
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Map.transform.Rotate(0, 0, 0);
+            //Map.transform.Rotate(0, 0, 0);
+            wantedRotation = Quaternion.Euler(Vector3.forward * (angle*2));
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Map.transform.Rotate(0, 0, angle * 2);
+            //Map.transform.Rotate(0, 0, angle * 2);
+            wantedRotation = Quaternion.Euler(Vector3.forward * 0);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            Map.transform.Rotate(0, 0, -angle);
+            //Map.transform.Rotate(0, 0, -angle);
+            wantedRotation = Quaternion.Euler(Vector3.forward * angle);
         }
+        transform.rotation = Quaternion.Lerp(transform.rotation, wantedRotation, Time.deltaTime * RotationSpeed);
     }
 }
