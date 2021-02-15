e93f3a4bb640628fa750cce1f88c621a71bcb988
peram <max.pera@hva.nl>
2021-02-15 22:31:25 +0100

    WinCondition+MainMenu

--- Assets/Scripts/DevilBehaviour.cs
+++ Assets/Scripts/DevilBehaviour.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class DevilBehaviour : MonoBehaviour
 {
-    
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            resetGame();
+        }
+    }
     void OnTriggerEnter2D(Collider2D Other)
     {
         if (Other.gameObject.CompareTag("Coin"))
         {
             Destroy(Other.gameObject);
         }
+
+        if(Other.gameObject.CompareTag("Goal"))
+        {
+            resetGame();
+            Debug.Log("help");
+        }
+    }
+
+    void resetGame()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
