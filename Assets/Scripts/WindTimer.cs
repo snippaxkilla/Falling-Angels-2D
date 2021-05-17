cd2bd177ff8dacf780c172e1ec552ededde6ae25
peram <max.pera@hva.nl>
2021-05-17 05:07:06 +0200

    change fix

--- Assets/Scripts/WindTimer.cs
+++ Assets/Scripts/WindTimer.cs
@@ -10,43 +10,53 @@ public class WindTimer : MonoBehaviour
     public bool WindTimeSwitch;
     ParticleSystem pSystem;
     AreaEffector2D areaEffector2;
+    ParticleSystemRenderer pSystemRenderer;
     // Start is called before the first frame update
     void Start()
     {
         pSystem = GetComponent<ParticleSystem>();
         areaEffector2 = GetComponent<AreaEffector2D>();
+        pSystemRenderer = GetComponent<ParticleSystemRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(isTimerUsed == true)
+        if (isTimerUsed == true)
         {
-            
+
             timer += Time.deltaTime;
 
-            if(timer >= timerOffset)
+            Debug.Log(WindTimeSwitch);
+
+            if (timer >= timerOffset)
             {
                 WindTimeSwitch = !WindTimeSwitch;
-            }
-
-            if (WindTimeSwitch == false)
-            {
                 timer = 0;
-                WindTimeSwitch = true;
-                pSystem.Play();
-                areaEffector2.enabled = true;
+
             }
 
             if (WindTimeSwitch == true)
             {
-                timer = 0;
-                WindTimeSwitch = false;
-                pSystem.Stop();
-                areaEffector2.enabled = false;
+
+                pSystemRenderer.enabled = true;
+                pSystem.Play();
+                areaEffector2.enabled = true;
+
             }
-            //Debug.Log(WindTimeSwitch);
-            //Debug.Log(timer);
+
+            if (WindTimeSwitch == false)
+            {
+                pSystemRenderer.enabled = false;
+                pSystem.Pause();
+                areaEffector2.enabled = false;
+
+            }
+
+            Debug.Log(WindTimeSwitch);
+            Debug.Log(timer);
         }
+
+
     }
 }
