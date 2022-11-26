e5fb5a062628b20f1b0a53d6c262f00750fcce8e
Pheubel <luca.guagliardo@live.nl>
2022-11-26 15:21:55 +0100

    Initialize analytics

---
+++ Assets/Scripts/Analytics/Initializer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+using UnityEngine;
+
+public class Initializer : MonoSingleton<Initializer>
+{
+    async void Start()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+            List<string> consentIdentifiers = await AnalyticsService.Instance.CheckForRequiredConsents();
+
+            Debug.Log("Analytics should be initialized.");
+        }
+        catch (ConsentCheckException e)
+        {
+            // Something went wrong when checking the GeoIP, check the e.Reason and handle appropriately.
+            Debug.LogError(e.Message);
+        }
+    }
+}
