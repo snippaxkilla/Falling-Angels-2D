103116d82dfa18a6adb8b32ead136d8c05f6c899
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2022-11-30 21:41:04 +0100

    bugfixes some levels had no sound

--- Assets/Scripts/Behaviour/WindSwitchCheck.cs
+++ Assets/Scripts/Behaviour/WindSwitchCheck.cs
@@ -6,40 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class WindSwitchCheck : MonoBehaviour
 {
-    
+
     public bool Switched;
     public float timer = 0;
     public string WindName;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime * 1;
-
-        
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-         
-
-                if (timer >= 2)
-                {
-                    Switched = !Switched;
-
-                    timer = 0;
-                }
-            
-
-            
+            if (timer >= 2)
+            {
+                Switched = !Switched;
+                timer = 0;
+            }
         }
     }
 }
